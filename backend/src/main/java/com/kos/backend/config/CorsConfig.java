55029f5e965fb4086432a4e050bd92d80679a89e
WTFSC <>
2024-01-06 15:46:53 +0800

    initial
    前后端分离架构初始化

---
+++ backend/src/main/java/com/kos/backend/config/CorsConfig.java
@@ -0,0 +1,46 @@
+package com.kos.backend.config;
+
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+// 在 Http 报头添加 "Access-Control-Allow-Origin" 等内容 从而允许 CORS(Cross Origin Resources Share) 允许跨域请求
+@Configuration
+public class CorsConfig implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        String origin = request.getHeader("Origin");
+        if(origin!=null) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+
+        String headers = request.getHeader("Access-Control-Request-Headers");
+        if(headers!=null) {
+            response.setHeader("Access-Control-Allow-Headers", headers);
+            response.setHeader("Access-Control-Expose-Headers", headers);
+        }
+
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
--- backend/src/main/java/com/kos/backend/controller/pk/IndexController.java
+++ backend/src/main/java/com/kos/backend/controller/pk/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/pk/")
 public class IndexController {
-    @RequestMapping("index")
+    @RequestMapping("index/")
     String index(){
         return "pk/index.html";
     }
